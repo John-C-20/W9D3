a148dcc0f1e565fb5b9e916a7c32fcae34667635
John <johncheung1997@gmail.com>
2021-02-24 15:26:49 -0500

    made border boxes for grid

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,11 +1,36 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el; 
+
+    this.setupBoard()
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    // const $ul = $('<ul> </ul>') 
+    
+    this.$el.append('<ul>')
+    
+    for (let i = 0; i < 9; i++) {
+      $('ul').append('<li> Boop </li>') 
+    };
+
+    $('ul').css({'display':'flex',
+     'width':'300px',
+    'flex-wrap':'wrap',
+      'border': '5px solid black'})
+
+    $('li').css({'width':'90px',
+      'height':'90px', 
+      'border':'5px solid black'})  
+
+    
+    
+  }
 }
 
 module.exports = View;
